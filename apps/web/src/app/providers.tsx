9564ee76763b5145553869d16c96865418be23a9
AlexiusMart <am-nf@yandex.ru>
2026-09-17 13:47:26 +0300

    chore(web): configure Tanstack Query

--- apps/web/src/app/providers.tsx
+++ apps/web/src/app/providers.tsx
@@ -1,6 +1,13 @@
 import type { PropsWithChildren } from "react"
 import { ConfigProvider } from "antd"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
+const queryClient = new QueryClient()
 
 export function AppProviders({ children }: PropsWithChildren) {
-  return <ConfigProvider>{children}</ConfigProvider>
+  return (
+    <ConfigProvider>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </ConfigProvider>
+  )
 }
